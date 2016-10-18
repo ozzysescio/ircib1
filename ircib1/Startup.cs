cf6768d8595bbaec40e2457314923e7c3a3cca52
epiii <gue.elfrianto@gmail.com>
2016-10-18 07:31:25 +0700

    ok

---
+++ ircib1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ircib1.Startup))]
+namespace ircib1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
